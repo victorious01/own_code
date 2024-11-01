52bfc673fb1f4c744172a5aa341bcf4ac2951f65
victorious01 <victorious.borbon@gmail.com>
2024-11-01 22:39:31 +0800

    push3

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use app\Models\Student;
 
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\RedirectResponse;
@@ -11,15 +13,16 @@ class StudentController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index(): View
     {
-        //
+        $students = Student::all();
+        return view ('student.index')->with('students', $students);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create(): Response
+    public function create(): View
     {
         //
     }
@@ -35,7 +38,7 @@ class StudentController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id): Response
+    public function show(string $id): View
     {
         //
     }
@@ -43,7 +46,7 @@ class StudentController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id): Response
+    public function edit(string $id): View
     {
         //
     }
@@ -59,7 +62,7 @@ class StudentController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id): Response
+    public function destroy(string $id): View
     {
         //
     }
